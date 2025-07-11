08143d463d5cdcebcffd97be209182dd18b5f180
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-07-11 16:17:41 +0700

    add task management screens and update navigation; implement TaskCreateScreen, TaskDetailScreen, TaskListScreen, and TaskUpdateScreen

---
+++ src/utils/localhost.js
@@ -0,0 +1,5 @@
+//export local host ip
+const LOCALHOST_IP_Vuong = 'http://192.168.1.22/suitecrm7/Api/V8';
+const LOCALHOST_IP_nhiet = 'http://192.168.101.7/suitecrm7/Api/V8';
+
+export const LOCALHOST_IP = LOCALHOST_IP_nhiet;
