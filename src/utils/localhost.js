beb5ba48654637ee321db3b3bf8aa5cba44d0793
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-07-25 16:25:26 +0700

    feat: add new language and view definitions files; update localhost IP configuration

--- src/utils/localhost.js
+++ src/utils/localhost.js
@@ -1,6 +1,6 @@
 //export local host ip
 const LOCALHOST_IP_Vuong = 'http://192.168.11.250/suitecrm7';
 const LOCALHOST_IP_nhiet = 'http://192.168.101.7/suitecrm7';
-const LOCALHOST_IP_CTY = 'http://192.168.1.12/suitecrm7';
+const LOCALHOST_IP_CTY = 'http://192.168.10.192/suitecrm7';
 
-export const LOCALHOST_IP = LOCALHOST_IP_nhiet; // Change this to switch between local environments
+export const LOCALHOST_IP = LOCALHOST_IP_CTY; // Change this to switch between local environments
