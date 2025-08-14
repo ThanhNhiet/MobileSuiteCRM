7ec7a42f36842b2951a490cf0b3b33d6b3fadda5
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2025-08-14 13:33:44 +0700

    update

--- src/utils/localhost.js
+++ src/utils/localhost.js
@@ -1,7 +1,7 @@
 //export local host ip
-const LOCALHOST_IP_Vuong = 'http://192.168.159.1/suitecrm7';
+const LOCALHOST_IP_Vuong = 'http://192.168.1.9/suitecrm7';
 const LOCALHOST_IP_nhiet = 'http://192.168.101.7/suitecrm7';
 const LOCALHOST_IP_CTY = 'http://192.168.10.192/suitecrm7';
 
-export const LOCALHOST_IP = LOCALHOST_IP_nhiet; // Change this to switch between local environments
+export const LOCALHOST_IP = LOCALHOST_IP_Vuong; // Change this to switch between local environments
 
